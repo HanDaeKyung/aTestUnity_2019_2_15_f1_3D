a8b2cef75bd4073b73809f0900fc2b18d7b029f9
ZQA <zkvpz@naver.com>
2019-12-15 12:22:52 +0900

    - 유니티 프로젝트 생성. - .gitignore 추가.

---
+++ Assets/_Test/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// .
+
+public class TestScript : MonoBehaviour
+{
+
+
+
+
+	private void Awake()
+	{
+
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+}
